bde9f23b37cb52d1f09baee0cb64bb2be98c4906
Laura Sophie Minneker <laura@minneker.de>
2018-11-04 20:35:52 +0100

    Yosay XD

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,5 +1,6 @@
 const Generator = require('yeoman-generator');
 const chalk = require('chalk');
+const yosay = require('yosay');
 
 
 module.exports = class extends Generator {
@@ -67,6 +68,6 @@ module.exports = class extends Generator {
     }
 
     end() {
-        this.log(chalk.yellow("All done!"));
+        this.log(yosay(chalk.yellow("All done!")));
     }
 };
